dfcb42a4790871b6867a8174704998efe113772b
TODO <TODO>
2024-02-21 17:16:00 +0100

    Implementation du code C++ partie 3 tp1

--- tp-ffi/py-logger/cpp/Logger.cpp
+++ tp-ffi/py-logger/cpp/Logger.cpp
@@ -6,16 +6,43 @@
 #include <sstream>
 
 void Logger::addItem(Level l, const std::string & m) {
-  // TODO implement Logger::addItem
+  std::tuple tuple1 = {l, m};
+  this->_items.push_back(tuple1);
 }
 
 std::string Logger::reportByAdded() const {
-  // TODO Logger::reportByAdded
-  return "TODO Logger::reportByAdded";
+  std::string texte = "**** report_by_added ****\n";
+  for(const std::tuple t : _items){
+    if(std::get<0>(t) == Level::Info){
+      texte += "[I] " + std::get<1>(t) + " \n";  
+    }
+    if(std::get<0>(t) == Level::Warning){
+      texte += "[W] " + std::get<1>(t) + " \n";  
+    }
+    if(std::get<0>(t) == Level::Error){
+      texte += "[E] " + std::get<1>(t) + " \n";  
+    }
+  }
+  return texte;
 }
 
 std::string Logger::reportByLevel() const {
-  // TODO Logger::reportByLevel
-  return "TODO Logger::reportByLevel";
+  std::string texte = "**** report_by_level ****\n";
+  for(const std::tuple t : _items){
+    if(std::get<0>(t) == Level::Info){
+      texte += "[I] " + std::get<1>(t) + " \n";  
+    }
+  }
+  for(const std::tuple t : _items){
+    if(std::get<0>(t) == Level::Warning){
+          texte += "[W] " + std::get<1>(t) + " \n";  
+    }
+  }
+  for(const std::tuple t : _items){
+    if(std::get<0>(t) == Level::Error){
+      texte += "[E] " + std::get<1>(t) + " \n";  
+    }
+  }
+  return texte;
 }
 
--- tp-ffi/py-logger/scripts/main1.py
+++ tp-ffi/py-logger/scripts/main1.py
@@ -1,11 +1,14 @@
-
+import Logger as log
 if __name__ == '__main__':
 
     print('TODO')
-
+    logger = log.Logger()
     # TODO create a logger and add some items
-
-    # TODO report_by_added
-
+    logger.add_item(log.Level.Info, "une info")
+    logger.add_item(log.Level.Error, "une erreur")
+    logger.add_item(log.Level.Info, "une autre info")
+    logger.add_item(log.Level.Warning, "un avertissement")
+    print(logger.report_by_added())
+    print(logger.report_by_level())
     # TODO report_by_level
 
