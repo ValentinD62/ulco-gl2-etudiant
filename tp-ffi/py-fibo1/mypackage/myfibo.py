e203ffc062439a50516a13274ef7fcb7fab7aa16
TODO <TODO>
2024-02-21 15:46:36 +0100

    Partie 1 TP1

--- tp-ffi/py-fibo1/mypackage/myfibo.py
+++ tp-ffi/py-fibo1/mypackage/myfibo.py
@@ -4,4 +4,10 @@ def fibo_naive(n: int):
     return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
 
 # TODO implement fibo_iterative
-
+def fibo_iterative(n: int):
+    compt = 0
+    a = 0
+    b = 1
+    for i in range(n):
+        a,b = b, a+b
+    return a
--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -1,9 +1,13 @@
-
+from mypackage.myfibo import *
 if __name__ == '__main__':
 
     print('TODO')
-    
     # TODO print fibo_naive(i) for i from 0 to 10
-
+    for i in range(0, 11):
+        print("fibo_naive(" + str(i) + ") = " + str(fibo_naive(i)))
+        
+    print ("")
     # TODO print fibo_iterative(i) for i from 0 to 10
-
+    for i in range(0, 11):
+        print("fibo_iterative(" + str(i) + ") = " + str(fibo_iterative(i)))
+    
