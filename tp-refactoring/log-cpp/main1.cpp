c34aab7f72b07a51ebb46a5b062ead26a81c19e4
TODO <TODO>
2024-03-11 15:41:54 +0100

    partie 4 TP3

--- tp-refactoring/log-cpp/main1.cpp
+++ tp-refactoring/log-cpp/main1.cpp
@@ -18,11 +18,24 @@ int mycompute(int v0) {
     return v2;
 }
 
+int mycomputeFile(int v0) {
+    std::string const nomFichier("../log1.txt");
+    std::ofstream monFlux(nomFichier.c_str());
+    monFlux << "add3 " + std::to_string(v0) << std::endl;
+    const int v1 = add3(v0);
+    monFlux << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    monFlux.close();
+    return v2;
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
 
     const int res = mycompute(18);
     std::cout << res << std::endl;
+    const int res2 = mycomputeFile(18);
+    std::cout << res2 << std::endl;
 
     return 0;
 }
--- tp-refactoring/log-cpp/main2.cpp
+++ tp-refactoring/log-cpp/main2.cpp
@@ -12,9 +12,40 @@ int mul2(int n) {
     return n*2;
 }
 
+int inj_dependance(int v0, logFunc_t fonction){
+    fonction ("add3 " + std::to_string(v0) + "\n");
+    const int v1 = add3(v0);
+    fonction ("mul2 " + std::to_string(v1) + "\n");
+    const int v2 = add3(v1);
+    return v2;
+}
+
+void f1(const std::string & str){
+    std::cout << str;
+}
+
+void f2(const std::string & str, std::ostream fichier){
+    fichier << str;
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
 
+    logFunc_t f1 = [](const std::string & str){
+        std::cout << str;
+    };
+    std::string const nomFichier("../log2.txt");
+    std::ofstream monFlux(nomFichier.c_str());
+    logFunc_t f2 = [& monFlux](const std::string & str){
+        monFlux << str;
+    };
+
+    const int res = inj_dependance(18, f1);
+    std::cout << res << std::endl;
+    
+    const int resF = inj_dependance(18, f2);
+    std::cout << resF << std::endl;
+    monFlux.close();
     return 0;
 }
 
