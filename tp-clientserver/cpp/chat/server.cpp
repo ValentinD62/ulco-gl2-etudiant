3962e6f89964f380135fca272837c9fd7547a2e8
TODO <TODO>
2024-04-05 17:03:59 +0200

    premiere partie chat

---
+++ tp-clientserver/cpp/chat/server.cpp
@@ -0,0 +1,36 @@
+#include <hv/WebSocketServer.h>
+
+#include "Net.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+int main() {
+    Net net;
+
+    hv::WebSocketService ws;
+    ws.onopen = [&net](const WebSocketChannelPtr& channel, const HttpRequestPtr& req) {
+        net.add(channel);
+        std::cout << "client connected" << std::endl;
+    };
+    ws.onmessage = [&net](const WebSocketChannelPtr& channel, const std::string& msg) {
+        auto sendInput = [&msg](const WebSocketChannelPtr & channel) {
+            channel->send(msg);
+        };
+        net.map(sendInput);
+    };
+    ws.onclose = [&net](const WebSocketChannelPtr& channel) {
+        net.del(channel);
+        std::cout << "client disconnected" << std::endl;
+    };
+
+    hv::WebSocketServer server;
+    server.registerWebSocketService(&ws);
+    server.setPort(9000);
+    std::cout << "waiting for clients..." << std::endl;
+    server.run();
+
+    return 0;
+}
+
