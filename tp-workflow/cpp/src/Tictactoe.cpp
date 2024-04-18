9696a090134976922c5635777c9410090669993a
TODO <TODO>
2024-04-18 13:12:56 +0200

    4e issue

--- tp-workflow/cpp/src/Tictactoe.cpp
+++ tp-workflow/cpp/src/Tictactoe.cpp
@@ -34,23 +34,28 @@ std::ostream & operator<<(std::ostream & os, const Jeu & jeu) {
 }
 
 void Jeu::victoire(){
+    int ok = 0;
     for (int i = 0; i < 3; i++){
         //Vérif ligne.
         if(_plateau[i][0] == _plateau[i][1] && _plateau[i][0] == _plateau[i][2]){
             if(_plateau[i][0] == Cell::Rouge){
                 _statut = Status::RougeGagne;
+                ok = 1;
             }
             else if (_plateau[i][0] == Cell::Vert){
                 _statut = Status::VertGagne;
+                ok = 1;
             }
         }
         //Vérif colonne.
         if(_plateau[0][i] == _plateau[1][i] && _plateau[0][i] == _plateau[2][i]){
             if(_plateau[0][i] == Cell::Rouge){
                 _statut = Status::RougeGagne;
+                ok = 1;
             }
             else if (_plateau[0][i] == Cell::Vert){
                 _statut = Status::VertGagne;
+                ok = 1;
             }
         }
     }
@@ -58,18 +63,35 @@ void Jeu::victoire(){
     if(_plateau[0][0] == _plateau[1][1] && _plateau[0][0] == _plateau[2][2]){
         if(_plateau[0][0] == Cell::Rouge){
                 _statut = Status::RougeGagne;
+                ok = 1;
         }
         else if (_plateau[0][0] == Cell::Vert){
             _statut = Status::VertGagne;
+            ok = 1;
         }
     }
     //Vérif diag 2.
     if(_plateau[0][2] == _plateau[1][1] && _plateau[0][2] == _plateau[2][0]){
         if(_plateau[0][2] == Cell::Rouge){
             _statut = Status::RougeGagne;
+            ok = 1;
         }
         else if (_plateau[0][2] == Cell::Vert){
             _statut = Status::VertGagne;
+            ok = 1;
+        }
+    }
+    if(ok == 0){//Si il n'y a pas de victoires.
+    int vide = 0;
+        for (int i=0;  i < 3; i++){
+            for (int j=0; j < 3; j++){
+                if(_plateau[i][j] == Cell::Vide){
+                    vide = 1;
+                }
+            }
+            if(vide == 0){
+                _statut = Status::Egalite;
+            }
         }
     }
 }
--- tp-workflow/cpp/src/TictactoeTest.cpp
+++ tp-workflow/cpp/src/TictactoeTest.cpp
@@ -55,6 +55,20 @@ TEST_CASE("Victoire verte"){
     jeu.jouer(1, 0);
     jeu.jouer(2, 2);
     jeu.jouer(1, 2);
-    std::cout << jeu;
     REQUIRE(jeu.getStatus() == Status::VertGagne);
+}
+
+TEST_CASE("Egalite"){
+    Jeu jeu;
+    jeu.jouer(0, 0);
+    jeu.jouer(0, 2);
+    jeu.jouer(0, 1);
+    jeu.jouer(1, 1);
+    jeu.jouer(1, 0);
+    jeu.jouer(1, 2);
+    jeu.jouer(2, 1);
+    jeu.jouer(2, 0);
+    jeu.jouer(2, 2);
+    std::cout << jeu;
+    REQUIRE(jeu.getStatus() == Status::Egalite);
 }
